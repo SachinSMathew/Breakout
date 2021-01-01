f59c865397fdb802da48f04c81e8882f35edd984
SachinSMathew <73436081+SachinSMathew@users.noreply.github.com>
2021-01-01 20:44:41 +0530

    callback

--- js/active.js
+++ js/active.js
@@ -27,19 +27,50 @@ $('document').ready(function(){
   
 })
 
-//======>  Mobile Menu Activation
-$('.main-navigation').meanmenu({
-    meanScreenWidth: "992",
-    meanMenuContainer: '.mobile-menu',
-    meanMenuClose: "<i class='icon icon-simple-remove'></i>",
-    meanMenuOpen: "<i class='icon icon-menu-34'></i>",
-    meanExpand: "",
-});
+// //======>  Mobile Menu Activation
+// $('.main-navigation').meanmenu({
+//     meanScreenWidth: "992",
+//     meanMenuContainer: '.mobile-menu',
+//     meanMenuClose: "<i class='icon icon-simple-remove'></i>",
+//     meanMenuOpen: "<i class='icon icon-menu-34'></i>",
+//     meanExpand: "",
+// });
+var fullNumber = 0
+const initialNumber = number => {
+    fullNumber = number;
+    console.log(fullNumber)
+}
+const token = "a2064791-13b2-4e57-bf5f-c21d1147f4ac";
+const sendMessage = event => {
+	event.preventDefault();
 
-var card = $(".card");
-
-$(document).on("mousemove",function(e) {  
-  var ax = -($(window).innerWidth()/2- e.pageX)/20;
-  var ay = ($(window).innerHeight()/2- e.pageY)/10;
-  card.attr("style", "transform: rotateY("+ax+"deg) rotateX("+ay+"deg);-webkit-transform: rotateY("+ax+"deg) rotateX("+ay+"deg);-moz-transform: rotateY("+ax+"deg) rotateX("+ay+"deg)");
-});
+	const message = {
+		Number: fullNumber
+	};
+	smtpJS(message);
+}
+const smtpJS = async message => {
+    document.getElementById('message').innerText = "Please wait.";
+    if (message.Number !== "" || message.Number.includes([a-z])){
+        try {
+            console.log(message)
+        await Email.send({
+            SecureToken : token,
+            To : "sachinsaji1999@gmail.com",
+            From : "support@fundfolio.in",
+            Subject : "Breakout leads",
+            Body : `This person has a confusion on breakout: Number: ${message.Number}`
+        }
+        );
+        document.getElementById('message').innerText = "Thank you! Your message is successfully sent :)";
+        } catch (e) {
+            document.getElementById('message').innerText = "Oops! Somthing went wrong :/";
+            console.log(e)
+    
+        }
+    }
+    else{
+        document.getElementById('message').innerText = "Please give your number";
+    }
+	document.getElementById('phone').value = '';
+};
