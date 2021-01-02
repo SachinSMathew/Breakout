864a54f6d151a97f3b6c9e452051daa9ea5d4db5
SachinSMathew <73436081+SachinSMathew@users.noreply.github.com>
2021-01-02 17:01:14 +0530

    Update active.js

--- js/active.js
+++ js/active.js
@@ -1,30 +1,30 @@
 $('document').ready(function(){
 
-     //======>  Pricing Toggle Functionality
+    //======>  Pricing Toggle Functionality
 
-    $("[class*='btn--toggle']").on('change',function(e){
-        
-        
-        var getTarget = $(this).attr('data-tab-target'); 
-        var inpSelect = $(this).children().children('input[type="checkbox"]');
+   $("[class*='btn--toggle']").on('change',function(e){
+       
+       
+       var getTarget = $(this).attr('data-tab-target'); 
+       var inpSelect = $(this).children().children('input[type="checkbox"]');
 
-        if($(inpSelect).is(':checked')){
-            if($(getTarget).hasClass('monthly')){
-                $(getTarget).removeClass('monthly');
-                $(getTarget).addClass('yearly');
-          
-            }
-        }else{
-            // $(getTarget).removeClass('monthly');
-            if($(getTarget).hasClass('yearly')){
-                $(getTarget).removeClass('yearly');
-                $(getTarget).addClass('monthly');
-          
-            }
-        }      
-    })
+       if($(inpSelect).is(':checked')){
+           if($(getTarget).hasClass('monthly')){
+               $(getTarget).removeClass('monthly');
+               $(getTarget).addClass('yearly');
+         
+           }
+       }else{
+           // $(getTarget).removeClass('monthly');
+           if($(getTarget).hasClass('yearly')){
+               $(getTarget).removeClass('yearly');
+               $(getTarget).addClass('monthly');
+         
+           }
+       }      
+   })
 
-  
+ 
 })
 
 // //======>  Mobile Menu Activation
@@ -37,40 +37,40 @@ $('document').ready(function(){
 // });
 var fullNumber = 0
 const initialNumber = number => {
-    fullNumber = number;
-    console.log(fullNumber)
+   fullNumber = number;
+   console.log(fullNumber)
 }
 const token = "a2064791-13b2-4e57-bf5f-c21d1147f4ac";
 const sendMessage = event => {
-	event.preventDefault();
+   event.preventDefault();
 
-	const message = {
-		Number: fullNumber
-	};
-	smtpJS(message);
+   const message = {
+       Number: fullNumber
+   };
+   smtpJS(message);
 }
 const smtpJS = async message => {
-    document.getElementById('message').innerText = "Please wait.";
-    if (message.Number !== "" || message.Number.includes([a-z])){
-        try {
-            console.log(message)
-        await Email.send({
-            SecureToken : token,
-            To : "breakout@fundfolio.in",
-            From : "support@fundfolio.in",
-            Subject : "Breakout leads",
-            Body : `This person has a confusion on breakout: Number: ${message.Number}`
-        }
-        );
-        document.getElementById('message').innerText = "Thank you! Your message is successfully sent :)";
-        } catch (e) {
-            document.getElementById('message').innerText = "Oops! Somthing went wrong :/";
-            console.log(e)
-    
-        }
-    }
-    else{
-        document.getElementById('message').innerText = "Please give your number";
-    }
-	document.getElementById('phone').value = '';
+   document.getElementById('message').innerText = "Please wait.";
+   if (message.Number !== "" || message.Number.includes([a-z])){
+       try {
+           console.log(message)
+       await Email.send({
+           SecureToken : token,
+           To : "breakout@fundfolio.in",
+           From : "support@fundfolio.in",
+           Subject : "Breakout leads",
+           Body : `This person has a confusion on breakout: Number: ${message.Number}`
+       }
+       );
+       document.getElementById('message').innerText = "Thank you! You'll recieve a callback soon :)";
+       } catch (e) {
+           document.getElementById('message').innerText = "Oops! Something went wrong :/";
+           console.log(e)
+   
+       }
+   }
+   else{
+       document.getElementById('message').innerText = "Please give your number";
+   }
+   document.getElementById('phone').value = '';
 };
